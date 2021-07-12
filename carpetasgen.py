2ddcc505920411b19073b89ff6eb639232f5cd0f
ElvisQ <evquispe@fi.uba.ar>
2021-07-12 13:47:17 -0300

    funcion opciones

--- carpetasgen.py
+++ carpetasgen.py
@@ -1,2 +1,18 @@
+
+
+def opciones()->int:
+    print('1)Generar carpetas localmente')
+    print('2)Generar carpetas en Google Drive')
+    opcion=input('\nIngrese una opcion: ')
+    while not opcion.isnumeric():
+        opcion=input('\nError, Vuelva a ingresar: ')
+    return int(opcion)
+
+
+
 def main_carpetas()->None:
-    pass
+    opcion=opciones()
+    if opcion==1:
+        pass
+    elif opcion==2:
+        pass
