2408f0d0551559ce286247df895c5d4c9e5ffbb4
NazarenoNapolitano <86419747+NazarenoNapolitano@users.noreply.github.com>
2021-07-17 21:57:14 -0300

    Add files via upload

---
+++ opciones.py
@@ -0,0 +1,36 @@
+import listado, creacion, subida, descarga
+
+def validar_opcion() -> int:
+    '''
+    Pide el ingreso de una opción y verifica que sea numérica.
+    '''
+    opcion = input()
+    while not opcion.isnumeric():
+        opcion = input("Entrada no valida. Debe ingresar un numero: ")
+    return int(opcion)
+
+def ejecutar_opcion(opcion: int, salida: bool) -> bool:
+    '''
+    Recibe una opción numérica y ejecuta una función según lo elegido.
+    '''
+    if opcion == 1:
+        listado.main_listado()
+    elif opcion == 2:
+        creacion.main_creacion()
+    elif opcion == 3:
+        subida.main_subida()
+    elif opcion == 4:
+        descarga.main_descarga()
+    elif opcion == 5:
+        sincronizar()
+    elif opcion == 6:
+        generar_carpetas()
+    elif opcion == 7:
+        actualizar_entregas()
+    elif opcion == 8:
+        salida = True
+    else:
+        print("Esa opcion no existe. Reingrese opcion: ")
+        opcion = validar_opcion()
+        ejecutar_opcion(opcion,salida)
+    return salida
