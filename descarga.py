a20bd1ba7835ca38e8b55b63add58ac143f1a000
NazarenoNapolitano <86419747+NazarenoNapolitano@users.noreply.github.com>
2021-07-19 20:21:50 -0300

    Update descarga.py

--- descarga.py
+++ descarga.py
@@ -2,43 +2,64 @@ import service_drive, io, os
 from googleapiclient.http import MediaIoBaseDownload
 
 RUTA_TRABAJO = os.getcwd()
+SERVICIO = service_drive.obtener_servicio()
+TIPOS_A_EXPORTAR = {'application/vnd.google-apps.presentation':'application/vnd.oasis.opendocument.presentation',
+                    'application/vnd.google-apps.document':'application/pdf',
+                    'application/vnd.google-apps.drawing':'image/jpeg',
+                    'application/vnd.google-apps.script':'application/vnd.google-apps.script+json',
+                    'application/vnd.google-apps.spreadsheet':'application/pdf',
+                    'application/vnd.google-apps.drawing':'image/jpeg',
+                    'application/vnd.google-apps.jam':'application/pdf',
+                    'application/vnd.google-apps.form':'application/pdf',
+                    'application/vnd.google-apps.site':'text/plain'
+}
 
-def descargar(nombre_archivo: str, id_archivo: str):
-    servicio = service_drive.obtener_servicio()
-    print(id_archivo)
-    print(nombre_archivo)
-    request = servicio.files().get_media(fileId=id_archivo)
+def descargar(nombre_archivo: str, id_archivo: str, tipo_archivo: str) -> None:
+    '''
+    Descarga el archivo solicitado por el usuario
+    '''
+    if tipo_archivo in TIPOS_A_EXPORTAR:
+        tipo_exportado = TIPOS_A_EXPORTAR[tipo_archivo]
+        request = SERVICIO.files().export_media(fileId=id_archivo,mimeType=tipo_exportado)
+    else:
+        request = SERVICIO.files().get_media(fileId=id_archivo)
     fh = io.BytesIO()
     downloader = MediaIoBaseDownload(fh, request)
     done = False
-    while done is False:
+    while not done:
         status, done = downloader.next_chunk()
         print("Download %d%%." % int(status.progress() * 100))
-
-#FUNCION DESCARGAR NO FUNCIONA TODAVÍA
-
+    fh.seek(0)
+    with open(os.path.join(RUTA_TRABAJO,nombre_archivo), 'wb') as f:
+        f.write(fh.read())
+        f.close()
+    print("Se ha descargado {0}".format(nombre_archivo))
 
 def verificar_existencia(nombre: str) -> tuple:
     '''
     Verifica que el nombre de archivo ingresado exista en el Drive del usuario. Si existe, llama a la función 'descargar'.
     '''
-    datos_archivos = (1,1)
-    servicio = service_drive.obtener_servicio()
-    archivos_coincidentes = servicio.files().list(fields='files(name,id)').execute()
-    archivo_encontrado = archivos_coincidentes['files']
-    for i in range(len(archivo_encontrado)):
-        if archivo_encontrado[i]['name'] == nombre:
-            datos_archivos = (nombre,archivo_encontrado[i]['id'])
-    print(datos_archivos)
+    datos_archivos = (1,1,1) #si datos queda (1,1,1) es porque no existe el archivo ingresado.
+    print("Verificando coincidencias, puede demorar...")
+    archivos_coincidentes = SERVICIO.files().list(fields='nextPageToken, files(id, name, mimeType, modifiedTime, parents)').execute()
+    lista_archivos = archivos_coincidentes.get('files')
+    nextPageToken = archivos_coincidentes.get('nextPageToken')
+    while nextPageToken:
+        archivos_coincidentes = SERVICIO.files().list(fields='nextPageToken, files(id, name, mimeType, modifiedTime, parents)',pageToken=nextPageToken).execute()
+        lista_archivos.extend(archivos_coincidentes.get('files'))
+        nextPageToken = archivos_coincidentes.get('nextPageToken')
+    for i in range(len(lista_archivos)):
+        if lista_archivos[i]['name'] == nombre:
+            datos_archivos = (nombre,lista_archivos[i]['id'],lista_archivos[i]['mimeType'])
     return datos_archivos
 
 def input_archivo() -> tuple:
     '''
     Pide al usuario ingresar un nombre de archivo. Si el usuario ingresa ENTER o una cadena de espacios, vuelve al menú. Caso contrario el programa procede a verificar la existencia del archivo.
     '''
-    datos_archivos = (0,0)
+    datos_archivos = (0,0,0) #si datos queda (0,0,0) se vuelve al menú.
     condicion_salida = True
-    archivo = input("\nIngrese el nombre del archivo que quiere descargar. Presione ENTER para volver al menú\n")
+    archivo = input("\nIngrese el nombre del archivo que quiere descargar. Presione ENTER o deje espacios para volver al menú.\n")
     for i in archivo:
         if i.isalnum():
             condicion_salida = False
@@ -48,11 +69,11 @@ def input_archivo() -> tuple:
         
 def main_descarga() -> None:
     resultados = input_archivo()
-    if resultados == (1,1):
+    if resultados == (1,1,1): #archivo inexistente
         respuesta = input("El archivo ingresado no existe. ¿Reintentar? (s/n) ")
         while respuesta != "s" and respuesta != "n":
             respuesta = input("Entrada no válida. ¿Reintentar? (s/n) ")
         if respuesta == "s":
             main_descarga()
-    elif resultados != (1,1) and resultados != (0,0):
-        descargar(resultados[0],resultados[1])
+    elif resultados != (1,1,1) and resultados != (0,0,0): #archivo encontrado
+        descargar(resultados[0],resultados[1],resultados[2])
